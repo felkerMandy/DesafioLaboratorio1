40f436f44f8d1c8e1ff8c2d1d16ec0ff272b4a97
felkerMandy <felker.dev@gmail.com>
2024-06-16 19:49:01 -0300

    ajustando pastas

---
+++ Etapa2/src/Loja.java
@@ -0,0 +1,92 @@
+public class Loja {
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+    private Endereco endereco;
+    private Data dataFundacao;
+
+    Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao){
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+        this.endereco = endereco;
+        this.dataFundacao = dataFundacao;
+
+    }
+
+    Loja(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFundacao){
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = -1;
+        this.endereco = endereco;
+        this.dataFundacao = dataFundacao;
+    }
+
+    public String getNome () {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios(){
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios){
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario(){
+        return salarioBaseFuncionario;
+    }
+
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario){
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Data getDataFundacao() {
+        return dataFundacao;
+    }
+
+    public void setDataFundacao(Data dataDeFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+
+    public char tamanhoDaLoja(){
+        if (quantidadeFuncionarios > 0 && quantidadeFuncionarios < 10 ){
+            return 'P';
+        }
+        else if (quantidadeFuncionarios >= 10 && quantidadeFuncionarios <= 30){
+            return 'M';
+        }
+        else {
+            return 'G';
+        }
+    }
+
+    public double gastosComSalario(){
+        if (salarioBaseFuncionario == -1){
+            return -1;
+        }
+        else{
+            double gastosComSalario = salarioBaseFuncionario * quantidadeFuncionarios;
+            return gastosComSalario;
+        }
+
+    }
+    @Override
+    public  String toString(){
+        return "Nome da Loja: " + this.nome + "Tamanho da Loja: " + this.tamanhoDaLoja() + "Gastos com Salário:"
+                + this.gastosComSalario() + "Endereço: " + this.endereco + "Data de Fundação: " + this.dataFundacao;
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# DesafioLaboratorio1
